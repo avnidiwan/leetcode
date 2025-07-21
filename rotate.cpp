c8a2328017146465b628e87d05924eaed8152f1e
avnidiwan <avnidiwan2845@gmail.com>
2025-07-21 22:59:20 +0530

    leetcode

---
+++ rotate.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k %= n; // In case k > n
+        vector<int> temp(n);
+
+        for (int i = 0; i < n; ++i) {
+            temp[(i + k) % n] = nums[i];
+        }
+
+        nums = temp;
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {1, 2, 3, 4, 5, 6, 7};
+    int k = 3;
+
+    sol.rotate(nums, k);
+
+    cout << "Rotated array: ";
+    for (int num : nums) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
