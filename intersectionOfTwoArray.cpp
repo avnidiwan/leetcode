6468d5cdbb7721470e9f05d70d54a2181034c3a8
avnidiwan <avnidiwan2845@gmail.com>
2025-07-23 23:30:11 +0530

    leetcode

---
+++ intersectionOfTwoArray.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <set>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        set<int> s;
+        for (int i = 0; i < nums1.size(); i++) {
+            for (int j = 0; j < nums2.size(); j++) {
+                if (nums1[i] == nums2[j])
+                    s.insert(nums2[j]);
+            }
+        }
+        return vector<int>(s.begin(), s.end());
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums1 = {1, 2, 2, 1};
+    vector<int> nums2 = {2, 2};
+    vector<int> result = sol.intersection(nums1, nums2);
+
+    cout << "Intersection: ";
+    for (int num : result) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
