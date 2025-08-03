71f77af3e114f4df2249b20f63c79aa98a602f80
avnidiwan <avnidiwan2845@gmail.com>
2025-08-03 22:15:52 +0530

    leetcode

---
+++ rotateBy90Degree.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm> // for reverse
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }
+};
+
+void printMatrix(const vector<vector<int>>& matrix) {
+    for (const auto& row : matrix) {
+        for (int val : row) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    vector<vector<int>> matrix = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    Solution sol;
+    cout << "Original Matrix:\n";
+    printMatrix(matrix);
+
+    sol.rotate(matrix);
+
+    cout << "\nRotated Matrix:\n";
+    printMatrix(matrix);
+
+    return 0;
+}
